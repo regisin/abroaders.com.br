5a69940065f8642fb8c30c43c60b1cf51f975719
regisin <regisin@users.noreply.github.com>
2022-01-12 04:26:11 +0000

    deploy: 6b846eb75502de53e10b25c7d90be4c958cfa78d

---
+++ _app/pages/contato/index.svelte.md-41ab44ca.js
@@ -0,0 +1,1 @@
+import{S as t,i as a,s as e}from"../../chunks/vendor-d3fabfa1.js";class l extends t{constructor(s){super();a(this,s,null,null,e,{})}}export{l as default};
---
+++ _app/pages/gpa/index.svelte-de99cf9e.js
@@ -0,0 +1,1 @@
+import{S as a,i as e,s as t}from"../../chunks/vendor-d3fabfa1.js";class l extends a{constructor(s){super();e(this,s,null,null,t,{})}}export{l as default};
