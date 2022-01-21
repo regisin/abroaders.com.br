47af9326b7c236bad9eb4e809ea0629598021d07
Paulo Alexandre Regis <regisin@gmail.com>
2022-01-20 22:42:05 -0600

    no idea...

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,37 +1,37 @@
-// import math from 'remark-math';
-// import rehype_katex from 'rehype-katex';
-// import katex from 'katex';
-// import visit from 'unist-util-visit';
+import math from 'remark-math';
+import rehype_katex from 'rehype-katex';
+import katex from 'katex';
+import visit from 'unist-util-visit';
 import attr from 'remark-attr';
 
-// const correct_hast_tree = () => (tree) => {
-// 	visit(tree, 'text', (node) => {
-// 		if (node.value.trim().startsWith('<')) {
-// 			node.type = 'raw';
-// 		}
-// 	});
-// };
+const correct_hast_tree = () => (tree) => {
+	visit(tree, 'text', (node) => {
+		if (node.value.trim().startsWith('<')) {
+			node.type = 'raw';
+		}
+	});
+};
 
-// const katex_blocks = () => (tree) => {
-// 	visit(tree, 'code', (node) => {
-// 		if (node.lang === 'math') {
-// 			const str = katex.renderToString(node.value, {
-// 				displayMode: true,
-// 				leqno: false,
-// 				fleqn: false,
-// 				throwOnError: true,
-// 				errorColor: '#cc0000',
-// 				strict: 'warn',
-// 				output: 'htmlAndMathml',
-// 				trust: false,
-// 				macros: { '\\f': '#1f(#2)' }
-// 			});
+const katex_blocks = () => (tree) => {
+	visit(tree, 'code', (node) => {
+		if (node.lang === 'math') {
+			const str = katex.renderToString(node.value, {
+				displayMode: true,
+				leqno: false,
+				fleqn: false,
+				throwOnError: true,
+				errorColor: '#cc0000',
+				strict: 'warn',
+				output: 'htmlAndMathml',
+				trust: false,
+				macros: { '\\f': '#1f(#2)' }
+			});
 
-// 			node.type = 'raw';
-// 			node.value = '{@html `' + str + '`}';
-// 		}
-// 	});
-// };
+			node.type = 'raw';
+			node.value = '{@html `' + str + '`}';
+		}
+	});
+};
 
 const config = {
   extensions: [".svelte.md", ".md", ".svx"],
@@ -40,10 +40,8 @@ const config = {
     dashes: "oldschool",
   },
 
-  remarkPlugins: [attr],
-  rehypePlugins: []
-//   remarkPlugins: [math, katex_blocks, attr],
-//   rehypePlugins: [correct_hast_tree, rehype_katex]
+  remarkPlugins: [math, katex_blocks, attr],
+  rehypePlugins: [correct_hast_tree, rehype_katex]
 };
 
 export default config;
